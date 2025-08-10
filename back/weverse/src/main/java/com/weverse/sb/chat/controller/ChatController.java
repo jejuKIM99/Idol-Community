8b3450cd6dc197b864c7bd6d0b87941c8bbbdbfe
junginn <athena1998@naver.com>
2025-08-10 16:22:06 +0900

    라이브 채팅 및 미디어 채팅 구현에 필요한 insert 추가

--- back/weverse/src/main/java/com/weverse/sb/chat/controller/ChatController.java
+++ back/weverse/src/main/java/com/weverse/sb/chat/controller/ChatController.java
@@ -23,6 +23,27 @@ public class ChatController {
 	
 	@Autowired
 	MediaService mediaService;
+	
+	/*
+	 * 
+	 INSERT INTO streaming 
+    (created_at, group_id, owner_artist_id, scheduled_at, streamer_artist_id, thumbnail, title, video_id)
+    VALUES
+    ('2025-08-09 10:00:00.000000', 1, 1, '2025-08-10 18:00:00.000000', 1, '/images/NewJeans/NewJeans_profile/NewJeans_live_01.jpg', 'First Streaming', 'https://youtu.be/wU2siJ2c5TA?si=4gGHQFcQmWfxEO--'),
+    ('2025-08-09 11:00:00.000000', 1, 2, '2025-08-11 19:00:00.000000', 2, '/images/NewJeans/NewJeans_profile/NewJeans_live_02.jpg', 'Second Streaming', 'https://youtu.be/aJqS7BY5XE4?si=-C5NucKka4jJZKjZ');
+
+
+    INSERT INTO uploaded_video 
+    (price, artist_id, group_id, scheduled_at, uploaded_at, description, thumbnail, title, video_url)
+    VALUES
+    (10000.00, 1, 1, '2025-08-15 18:00:00.000000', '2025-08-09 10:00:00.000000', 'Live concert performance of Newjeans', '/images/NewJeans/NewJeans_profile/NewJeans_media_01.jpg', 'NewJeans Live Concert', 'https://youtu.be/wLApx-mWc5Y?si=GMJKox7vJz_bmh6Y'),
+    (10000.00, 2, 1, NULL, '2025-08-09 11:00:00.000000', 'Behind-the-scenes footage from recent album recording', '/images/NewJeans/NewJeans_profile/NewJeans_media_02.jpg', 'NewJeans Album Recording', 'https://youtu.be/yWvzZW_g5b0?si=F-zJKDtBEoQGriNS'),
+    (10000.00, 3, 1, '2025-08-20 20:00:00.000000', '2025-08-08 09:30:00.000000', 'Exclusive interview with Newjeans', '/images/NewJeans/NewJeans_profile/NewJeans_media_03.jpg', 'NewJeans Exclusive Interview', 'https://youtu.be/WOlVl32HaoY?si=slhAAUnD80tJQImh'),
+    (10000.00, 4, 1, NULL, '2025-08-07 15:45:00.000000', 'Special fan meeting highlights', '/images/NewJeans/NewJeans_profile/NewJeans_media_04.jpg', 'NewJeans Fan Meeting Highlights', 'https://youtu.be/gY8_MPI3p4Q?si=hacg_d5ES8eMsVPO');
+
+	 * 
+	 */
+	
 
     @PostMapping("/messages/live")
     public ResponseEntity<String> saveLiveMessage(@RequestBody LiveChatMessageDTO chatMessage) {
