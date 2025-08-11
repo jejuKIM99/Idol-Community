aaf802344dd691f8bdd37eab5e3f1927c540bf32
KimHS <kkspooto@gmail.com>
2025-08-11 12:18:30 +0900

    Update StreamingDTO.java

--- back/weverse/src/main/java/com/weverse/sb/media/dto/StreamingDTO.java
+++ back/weverse/src/main/java/com/weverse/sb/media/dto/StreamingDTO.java
@@ -1,31 +1,40 @@
 package com.weverse.sb.media.dto;
 
+import com.weverse.sb.artist.entity.Artist;
+import com.weverse.sb.artist.entity.Group;
+import com.weverse.sb.media.entity.Streaming;
 import java.time.LocalDateTime;
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-@ToString
-public class StreamingDTO {
-	
+@NoArgsConstructor
+@AllArgsConstructor
+public class StreamingDto {
     private Long streamingId;
-    private Long owner;
-    private Long streamer;
+    private Artist owner;
+    private Artist streamer;
+    private String videoId;
     private String title;
     private String thumbnail;
     private LocalDateTime createdAt;
     private LocalDateTime scheduledAt;
-    private Long groupId;
-    private String groupName;
-    
-    private List<StreamingDTO> streamList;
+    private Group group;
 
+    public static StreamingDto fromEntity(Streaming streaming) {
+        return StreamingDto.builder()
+            .streamingId(streaming.getStreamingId())
+            .owner(streaming.getOwner())
+            .streamer(streaming.getStreamer())
+            .videoId(streaming.getVideoId())
+            .title(streaming.getTitle())
+            .thumbnail(streaming.getThumbnail())
+            .createdAt(streaming.getCreatedAt())
+            .scheduledAt(streaming.getScheduledAt())
+            .group(streaming.getGroup())
+            .build();
+    }
 }
